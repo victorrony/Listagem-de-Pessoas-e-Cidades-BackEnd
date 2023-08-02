280c13a75030ff95d07dc9a5dccf1d5d099b0f06
Victor Rony <victorrony89@gmail.com>
2023-08-02 16:40:36 -0100

    added code style config

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,8 +3,10 @@ import express from 'express';
 
 const server = express();
 
-server.get('/', (req, res) => {
+
+
+server.get('/', (_, res) => {
    return res.send('Hello World');
-})
+});
 
 export { server};
