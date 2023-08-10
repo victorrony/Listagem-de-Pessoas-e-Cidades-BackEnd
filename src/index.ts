64a7942daea23088813c5b469ca266a28f61600c
Victor Rony <victorrony89@gmail.com>
2023-08-10 10:31:17 -0100

    add seed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
+import { Knex } from './server/database/knex';
 import { server } from './server/server';
 
 
 server.listen(process.env.PORT || 3333, () => console.log('App rodando!'));
+
+
+// if (process.env.IS_LOCALHOST  !== 'true') {
+//   console.log('Rodando migration');
+
+//   Knex.migrate
+//     .latest();
+//     .then(() => {
+//       Knex.seed.run()
+//         .then(() => startServer())
+//         .catch(console.log);
+//     })
+//     .catch(console.log)
+// }else {
+//   startServer();
+// }
