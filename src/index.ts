a6d6477954d1cbe10601961d916c70a7ccfcebe8
victorrony <victor.fernandes@sintaxy.com>
2024-08-20 14:36:49 -0100

    my last update

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
-// import { Knex } from './server/database/knex';
-import { server } from "./api/server";
+import { Knex } from "./server/database/knex";
+import { server } from "./server/server";
 
-const PORTA = 3333;
+const startServer = () => {
+  server.listen(process.env.PORT || 3335, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 3335}`);
+  });
+};
 
-server.listen(process.env.PORT || 3333, () =>
-  console.log(`App rodando! na porta ${PORTA}`)
-);
+if (process.env.IS_LOCALHOST !== "true") {
+  console.log("Rodando migrations");
 
-// if (process.env.IS_LOCALHOST  !== 'true') {
-//   console.log('Rodando migration');
-
-//   Knex.migrate
-//     .latest();
-//     .then(() => {
-//       Knex.seed.run()
-//         .then(() => startServer())
-//         .catch(console.log);
-//     })
-//     .catch(console.log)
-// }else {
-//   startServer();
-// }
+  Knex.migrate
+    .latest()
+    .then(() => {
+      Knex.seed
+        .run()
+        .then(() => startServer())
+        .catch(console.log);
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
---
+++ src/server/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import "./shared/services/TranslationsYup";
+import { router } from "./routes/index";
+
+const server = express();
+
+server.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
+  })
+);
+
+server.use(express.json());
+
+server.use(router);
+
+export { server };
