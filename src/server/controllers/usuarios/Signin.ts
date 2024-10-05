9f06fe8c14816d24bf269a762ffbd0cb53d587b7
victorrony <victor.fernandes@sintaxy.com>
2024-10-05 10:04:27 -0100

    creta regster

--- src/server/controllers/usuarios/Signin.ts
+++ src/server/controllers/usuarios/Signin.ts
@@ -18,48 +18,6 @@ export const signInValidation = validation((getSchema) => ({
   ),
 }));
 
-// export const signIn = async (
-//   req: Request<{}, {}, IBodyProps>,
-//   res: Response
-// ) => {
-//   const { email, senha } = req.body;
-
-//   const usuario = await UsuariosProvider.getByEmail(email);
-//   //console.log(usuario);
-
-//   if (usuario instanceof Error) {
-//     return res.status(StatusCodes.UNAUTHORIZED).json({
-//       errors: {
-//         default: 'Email invalidos',
-//       },
-//     });
-//   }
-
-//   const passwordMatch = await PasswordCrypto.verifyPassword(
-//     senha,
-//     usuario.senha
-//   );
-
-//   if (!passwordMatch) {
-//     return res.status(StatusCodes.UNAUTHORIZED).json({
-//       errors: {
-//         default: 'Senha invalidos',
-//       },
-//     });
-//   } else {
-//     const accessToken = JwtService.sign({ uid: usuario.id });
-//     if (accessToken === 'JWT_SECRET_NOT_FOUND') {
-//       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-//         errors: {
-//           default: 'Erro ao gerar o token de acesso',
-//         },
-//       });
-//     }
-
-//     return res.status(StatusCodes.OK).json({ accessToken });
-//   }
-// };
-
 export const signIn = async (
   req: Request<{}, {}, IBodyProps>,
   res: Response
